ef0fc71956d3808649a22562eb703fd336d72070
Eli Pavlov <pavlov.eli@gmail.com>
2024-01-09 15:49:05 +0200

    Update master.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -43,10 +43,10 @@ elif [ "$pod_network_plugin" == "Cilium" ]; then
     CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
     CLI_ARCH=amd64
     if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
-    sudo curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
-    sudo sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+    curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+    sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
     sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
-    sudo rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+    rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
     export KUBECONFIG=/home/vagrant/.kube/config
     su - vagrant -c "cilium install --version 1.14.5"
 else
