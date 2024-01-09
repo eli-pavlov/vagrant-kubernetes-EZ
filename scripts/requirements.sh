d3ac3b45483bd258c33d9ada73035d720529dcd5
Eli Pavlov <pavlov.eli@gmail.com>
2024-01-09 15:00:11 +0200

    Update requirements.sh

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -75,7 +75,7 @@ sudo apt-get update
 sudo apt-get install containerd.io -y
 
 # Install apt-transport-https pkg
-apt-get update && apt-get sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.26/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 # Configuring the systemd cgroup drive:
